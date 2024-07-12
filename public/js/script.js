d12d1274b6ec16380d06f64d54c9e4cca364f9f1
Xvannn 文 <95283773+Xvannn07@users.noreply.github.com>
2024-07-12 11:43:33 +0800

    Add Supported Link Youtube Shorts

--- public/js/script.js
+++ public/js/script.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const url_regex = /https:\/\/youtu(?:be\.com|\.be\/)/
 
       if(!url_regex.test(videoUrl)) {
-        await costumAlert("THE LINK YOU ENTERETED DOES NOT MATCH!!!", { status: "fail"});
+        await costumAlert("The Link You Entreted Does Not Match!!!", { status: "fail"});
         loadingElement.style.display = 'none';
       }
 
@@ -46,72 +46,81 @@ document.addEventListener('DOMContentLoaded', () => {
             })
             .then(resp => resp.data)
             .then(async (htmls) => {
+                
                 loadingElement.style.display = 'none';
                 alertContainer.style.display = 'none';
-                resultElement.focus();
-                resultElement.innerHTML = htmls;
-                await costumAlert("succes", { status: "succes"})
+                
 
-                const videoBtn = document.getElementById('video-btn');
-                const audioBtn = document.getElementById('audio-btn');
-                const videoTable = document.getElementById('video-table');
-                const audioTable = document.getElementById('audio-table');
-                videoTable.classList.remove('hidden');
-
-                videoBtn.addEventListener('click', () => {
+                if(data.status == "error") {
+                    await costumAlert(data.message, { status: "fail"});
+                }
+                if(data.status == "oke") {             
+                    resultElement.innerHTML = htmls;
+                    resultElement.focus();
+                    await costumAlert("succes", { status: "succes"});
+    
+                    const videoBtn = document.getElementById('video-btn');
+                    const audioBtn = document.getElementById('audio-btn');
+                    const videoTable = document.getElementById('video-table');
+                    const audioTable = document.getElementById('audio-table');
                     videoTable.classList.remove('hidden');
-                    audioTable.classList.add('hidden');
-                });
-              
-                audioBtn.addEventListener('click', () => {
-                    audioTable.classList.remove('hidden');
-                    videoTable.classList.add('hidden');
-                });
-
-                document.getElementById('thumb').src = data.videoDetail.thumbnails[2].url || data.videoDetail.thumbnails[1].url || data.videoDetail.thumbnails[0].url;
-                document.getElementById('title').textContent = "Title: " + data.videoDetail.title;
-                document.getElementById('duration').textContent = "Duration: " + await convertToTime(data.videoDetail.lengthSeconds) || "Not Found";
-                document.getElementById('author').textContent = "Author: " + data.videoDetail.author.name || "Not Found";
-                document.getElementById('channel').textContent = "Channel: " + data.videoDetail.author.user || "Not Found";
-                document.getElementById('upload').textContent = "Upload Date: " + await convertDateFormat(data.videoDetail.uploadDate) || "Not Found";
-                document.getElementById('views').textContent = "Views: " + await convertViews(data.videoDetail.viewCount) || "Not Found";
- 
-                // video result
-                const video_tabel = document.getElementById('video-table');
-                const tbodyA = document.createElement('tbody');
-                video_tabel.appendChild(tbodyA);
-                data.format_video.forEach(element => {
-                  if(element.mimeType.split(";")[0] == "video/mp4" || element.mimeType.split(";")[0] == "video/webm") {
-                    let tr = document.createElement('tr');
-                    tr.innerHTML = `
-                          <td>${element.qualityLabel}</td>
-                          <td>${byteToMegabyte(element.contentLength)}</td>
-                          <td>${element.mimeType.split(";")[0]}</td>
-                          <td><a target="_blank" href=${element.url}><button type="button" class="btn btn-success"><lord-icon src="https://cdn.lordicon.com/xcrjfuzb.json" trigger="loop" delay="1000" style="width:15px;height:15px;margin-right:10px;display:inline-block"> </lord-icon>Download</button></a></td>
-                    `;
-                    tbodyA.appendChild(tr);
-                  };
-                });
-
-                // audio result
-                const audio_tabel = document.getElementById('audio-table');
-                const tbodyB = document.createElement('tbody');
-                audio_tabel.appendChild(tbodyB);
-                data.format_video.forEach(element => {
-                  if(element.mimeType.split(";")[0] == "audio/mp4" || element.mimeType.split(";")[0] == "audio/webm") {
-                    let tr = document.createElement('tr');
-                    tr.innerHTML = `
-                          <td>${element.audioQuality}</td>
-                          <td>${byteToMegabyte(element.contentLength)}</td>
-                          <td>${element.mimeType.split(";")[0]}</td>
-                          <td><a target="_blank" href=${element.url}><button type="button" class="btn btn-success"><lord-icon src="https://cdn.lordicon.com/xcrjfuzb.json" trigger="loop" delay="1000" style="width:15px;height:15px;margin-right:10px;display:inline-block"> </lord-icon>Download</button></a></td>
-                    `;
-                    tbodyB.appendChild(tr);
-                  };
-                });
+    
+                    videoBtn.addEventListener('click', () => {
+                        videoTable.classList.remove('hidden');
+                        audioTable.classList.add('hidden');
+                    });
+                  
+                    audioBtn.addEventListener('click', () => {
+                        audioTable.classList.remove('hidden');
+                        videoTable.classList.add('hidden');
+                    });
+    
+                    document.getElementById('thumb').src = data.videoDetail.thumbnails[2].url || data.videoDetail.thumbnails[1].url || data.videoDetail.thumbnails[0].url;
+                    document.getElementById('title').textContent = "Title: " + data.videoDetail.title;
+                    document.getElementById('duration').textContent = "Duration: " + await convertToTime(data.videoDetail.lengthSeconds) || "Not Found";
+                    document.getElementById('author').textContent = "Author: " + data.videoDetail.author.name || "Not Found";
+                    document.getElementById('channel').textContent = "Channel: " + data.videoDetail.author.user || "Not Found";
+                    document.getElementById('upload').textContent = "Upload Date: " + await convertDateFormat(data.videoDetail.uploadDate) || "Not Found";
+                    document.getElementById('views').textContent = "Views: " + await convertViews(data.videoDetail.viewCount) || "Not Found";
+     
+                    // video result
+                    const video_tabel = document.getElementById('video-table');
+                    const tbodyA = document.createElement('tbody');
+                    video_tabel.appendChild(tbodyA);
+                    data.format_video.forEach(element => {
+                      if(element.mimeType.split(";")[0] == "video/mp4" || element.mimeType.split(";")[0] == "video/webm") {
+                        let tr = document.createElement('tr');
+                        tr.innerHTML = `
+                              <td>${element.qualityLabel}</td>
+                              <td>${byteToMegabyte(element.contentLength)}</td>
+                              <td>${element.mimeType.split(";")[0]}</td>
+                              <td><a target="_blank" href=${element.url}><button type="button" class="btn btn-success"><lord-icon src="https://cdn.lordicon.com/xcrjfuzb.json" trigger="loop" delay="1000" style="width:15px;height:15px;margin-right:10px;display:inline-block"> </lord-icon>Download</button></a></td>
+                        `;
+                        tbodyA.appendChild(tr);
+                      };
+                    });
+    
+                    // audio result
+                    const audio_tabel = document.getElementById('audio-table');
+                    const tbodyB = document.createElement('tbody');
+                    audio_tabel.appendChild(tbodyB);
+                    data.format_video.forEach(element => {
+                      if(element.mimeType.split(";")[0] == "audio/mp4" || element.mimeType.split(";")[0] == "audio/webm") {
+                        let tr = document.createElement('tr');
+                        tr.innerHTML = `
+                              <td>${element.audioQuality}</td>
+                              <td>${byteToMegabyte(element.contentLength)}</td>
+                              <td>${element.mimeType.split(";")[0]}</td>
+                              <td><a target="_blank" href=${element.url}><button type="button" class="btn btn-success"><lord-icon src="https://cdn.lordicon.com/xcrjfuzb.json" trigger="loop" delay="1000" style="width:15px;height:15px;margin-right:10px;display:inline-block"> </lord-icon>Download</button></a></td>
+                        `;
+                        tbodyB.appendChild(tr);
+                      };
+                    });
+                };
             });
           });
         } catch (error) {
+          await costumAlert("An error occurred on the server, please wait a moment, we will fix it immediately", { status: "fail"});
           console.error(`Error fetching result: ${error}`);
         }
       }
